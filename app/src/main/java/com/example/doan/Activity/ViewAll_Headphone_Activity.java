37ca8faf2f460ec798a77af71a537f114d8bb793
bany <quochung291220044@gmail.com>
2024-03-05 10:32:20 +0700

    QH commit

---
+++ app/src/main/java/com/example/doan/Activity/ViewAll_Headphone_Activity.java
@@ -0,0 +1,98 @@
+package com.example.doan.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.doan.Adapter.PopularListAdapter;
+import com.example.doan.R;
+import com.example.doan.model.PopularModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ViewAll_Headphone_Activity extends AppCompatActivity {
+
+    RecyclerView recyclerViewAll;
+    PopularListAdapter popularListAdapter;
+    List<PopularModel> arr_all;
+
+    SearchView searchView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_all_headphone);
+
+        addControls();
+        loadData();
+    }
+
+    private void loadData() {
+        arr_all.add(new PopularModel("Airpod 2",R.drawable.airpod2,4.79,10,4.9));
+        arr_all.add(new PopularModel("Airpod 3",R.drawable.airpod3,5.29,12,4.7));
+        arr_all.add(new PopularModel("Airpod Pro",R.drawable.airpod_pro,6.39,10,4.5));
+        arr_all.add(new PopularModel("Airpod Max",R.drawable.airpodhd,10.99,10,4.0));
+        arr_all.add(new PopularModel("Airpod 2",R.drawable.airpod2,4.79,10,4.9));
+        arr_all.add(new PopularModel("Airpod 3",R.drawable.airpod3,5.29,12,4.7));
+        arr_all.add(new PopularModel("Airpod Pro",R.drawable.airpod_pro,6.39,10,4.5));
+        arr_all.add(new PopularModel("Airpod Max",R.drawable.airpodhd,10.99,10,4.0));
+        arr_all.add(new PopularModel("Airpod 2",R.drawable.airpod2,4.79,10,4.9));
+        arr_all.add(new PopularModel("Airpod 3",R.drawable.airpod3,5.29,12,4.7));
+        arr_all.add(new PopularModel("Airpod Pro",R.drawable.airpod_pro,6.39,10,4.5));
+        arr_all.add(new PopularModel("Airpod Max",R.drawable.airpodhd,10.99,10,4.0));
+    }
+
+    private void addControls() {
+        recyclerViewAll=findViewById(R.id.view7);
+        searchView=findViewById(R.id.search);
+
+
+        arr_all=new ArrayList<>();
+        popularListAdapter = new PopularListAdapter(ViewAll_Headphone_Activity.this,arr_all);
+        recyclerViewAll.setAdapter(popularListAdapter);
+
+
+        GridLayoutManager gridLayoutManager=new GridLayoutManager(this,2);
+        recyclerViewAll.setLayoutManager(gridLayoutManager);
+
+        //SEARCH
+        searchView.clearFocus();
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                searchList(newText);
+                return true;
+            }
+        });
+    }
+
+    private void searchList(String text) {
+        ArrayList<PopularModel> dataSearchList =new ArrayList<>();
+        for (PopularModel data: arr_all){
+            if(data.getTxtTensp().toLowerCase().contains(text.toLowerCase())){
+                dataSearchList.add(data);
+            }
+        }
+        if(dataSearchList.isEmpty()){
+            Toast.makeText(this,"Không tìm thấy",Toast.LENGTH_SHORT).show();
+        } else {
+            popularListAdapter.setSearchList(dataSearchList);
+        }
+
+    }//SEARCH
+
+    public void QuayVe(View view){
+        finish();
+    }
+}
