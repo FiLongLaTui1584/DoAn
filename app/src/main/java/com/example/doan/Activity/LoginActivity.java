bc39342d8183f9b7792d20d37a879fb06a5cca2f
Phi Long <philongt474@gmail.com>
2024-03-11 23:46:53 +0700

    PhiLong commit (có database login, register)

--- app/src/main/java/com/example/doan/Activity/LoginActivity.java
+++ app/src/main/java/com/example/doan/Activity/LoginActivity.java
@@ -1,10 +1,13 @@
 package com.example.doan.Activity;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
@@ -12,14 +15,22 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.doan.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import org.jetbrains.annotations.NotNull;
 
 public class LoginActivity extends AppCompatActivity {
 
     ImageButton btnLogin;
     TextView register;
     EditText edtEmail,edtPass;
+
+    FirebaseAuth mAuth;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
@@ -30,35 +41,44 @@ public class LoginActivity extends AppCompatActivity {
     private void addEvents() {
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                Drawable icERR=getResources().getDrawable(R.drawable.ic_error);
-                icERR.setBounds(0,0,icERR.getIntrinsicWidth(),icERR.getIntrinsicHeight());
-                String email=edtEmail.getText().toString().trim();
-                String pass=edtPass.getText().toString().trim();
-                if(email.isEmpty()){
-                    edtEmail.setCompoundDrawables(null,null,icERR,null);
-                    edtEmail.setError("Vui lòng nhập email!",icERR);
-                    Toast.makeText(LoginActivity.this,"Đăng nhập thất bại",Toast.LENGTH_SHORT).show();
-                }
-                if(pass.isEmpty()){
-                    edtPass.setCompoundDrawables(null,null,icERR,null);
-                    edtPass.setError("Vui lòng nhập pass!",icERR);
-                    Toast.makeText(LoginActivity.this,"Đăng nhập thất bại",Toast.LENGTH_SHORT).show();
-                }
-                if(!email.isEmpty()&&!pass.isEmpty()){
-                    edtEmail.setCompoundDrawables(null,null,null,null);
-                    edtPass.setCompoundDrawables(null,null,null,null);
-                    Toast.makeText(LoginActivity.this,"Đăng nhập thành công",Toast.LENGTH_SHORT).show();
-                    Intent i=new Intent(LoginActivity.this, MainActivity.class);
-                    startActivity(i);
-                }
-            }
+            public void onClick(View v) {login();}
         });
 
 
         register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {moRegister();}
+        });
+    }
+
+    private void login() {
+        Drawable icERR=getResources().getDrawable(R.drawable.ic_error);
+        icERR.setBounds(0,0,icERR.getIntrinsicWidth(),icERR.getIntrinsicHeight());
+        String email=edtEmail.getText().toString();
+        String pass=edtPass.getText().toString();
+        if(TextUtils.isEmpty(email)){
+            edtEmail.setCompoundDrawables(null,null,icERR,null);
+            edtEmail.setError("Vui lòng nhập email!",icERR);
+            Toast.makeText(LoginActivity.this,"Đăng nhập thất bại",Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if(TextUtils.isEmpty(pass)){
+            edtPass.setCompoundDrawables(null,null,icERR,null);
+            edtPass.setError("Vui lòng nhập pass!",icERR);
+            Toast.makeText(LoginActivity.this,"Đăng nhập thất bại",Toast.LENGTH_SHORT).show();
+            return;
+        }
+        mAuth.signInWithEmailAndPassword(email,pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull @NotNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    Toast.makeText(getApplicationContext(),"Đăng nhập thành công",Toast.LENGTH_SHORT).show();
+                    Intent i=new Intent(LoginActivity.this, MainActivity.class);
+                    startActivity(i);
+                }else {
+                    Toast.makeText(getApplicationContext(),"Đăng nhập thất bại",Toast.LENGTH_SHORT).show();
+                }
+            }
         });
     }
 
@@ -72,5 +92,7 @@ public class LoginActivity extends AppCompatActivity {
         edtEmail=findViewById(R.id.edtEmail);
         edtPass=findViewById(R.id.edtPass);
         register=(TextView)findViewById(R.id.btn_register);
+
+        mAuth=FirebaseAuth.getInstance();
     }
 }
--- app/src/main/java/com/example/doan/Activity/RegisterActivity.java
+++ app/src/main/java/com/example/doan/Activity/RegisterActivity.java
@@ -1,23 +1,34 @@
 package com.example.doan.Activity;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.doan.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import org.jetbrains.annotations.NotNull;
 
 public class RegisterActivity extends AppCompatActivity {
 
     ImageButton btnLogin;
-    EditText edtName,edtEmail,edtPass;
+    EditText edtEmail,edtPass;
+    FirebaseAuth mAuth;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
@@ -29,29 +40,37 @@ public class RegisterActivity extends AppCompatActivity {
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Drawable icERR=getResources().getDrawable(R.drawable.ic_error);
-                icERR.setBounds(0,0,icERR.getIntrinsicWidth(),icERR.getIntrinsicHeight());
-                String name=edtName.getText().toString().trim();
-                String email=edtEmail.getText().toString().trim();
-                String pass=edtPass.getText().toString().trim();
-                if(name.isEmpty()){
-                    edtName.setCompoundDrawables(null,null,icERR,null);
-                    edtName.setError("Vui lòng nhập tên!",icERR);
-                }
-                if(email.isEmpty()){
-                    edtEmail.setCompoundDrawables(null,null,icERR,null);
-                    edtEmail.setError("Vui lòng nhập email!",icERR);
-                }
-                if(pass.isEmpty()){
-                    edtPass.setCompoundDrawables(null,null,icERR,null);
-                    edtPass.setError("Vui lòng nhập pass!",icERR);
-                }
-                if(!name.isEmpty()&&!email.isEmpty()&&!pass.isEmpty()){
-                    edtName.setCompoundDrawables(null,null,null,null);
-                    edtEmail.setCompoundDrawables(null,null,null,null);
-                    edtPass.setCompoundDrawables(null,null,null,null);
+                register();
+            }
+        });
+    }
+
+    private void register() {
+        Drawable icERR=getResources().getDrawable(R.drawable.ic_error);
+        icERR.setBounds(0,0,icERR.getIntrinsicWidth(),icERR.getIntrinsicHeight());
+        String email=edtEmail.getText().toString();
+        String pass=edtPass.getText().toString();
+        if(TextUtils.isEmpty(email)){
+            edtEmail.setCompoundDrawables(null,null,icERR,null);
+            edtEmail.setError("Vui lòng nhập email!",icERR);
+            Toast.makeText(RegisterActivity.this,"Đăng kí thất bại",Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if(TextUtils.isEmpty(pass)){
+            edtPass.setCompoundDrawables(null,null,icERR,null);
+            edtPass.setError("Vui lòng nhập pass!",icERR);
+            Toast.makeText(RegisterActivity.this,"Đăng kí thất bại",Toast.LENGTH_SHORT).show();
+            return;
+        }
+        mAuth.createUserWithEmailAndPassword(email,pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull @NotNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    Toast.makeText(getApplicationContext(),"Đăng kí thành công",Toast.LENGTH_SHORT).show();
                     Intent i=new Intent(RegisterActivity.this, LoginActivity.class);
                     startActivity(i);
+                }else {
+                    Toast.makeText(getApplicationContext(),"Đăng kí thất bại",Toast.LENGTH_SHORT).show();
                 }
             }
         });
@@ -59,8 +78,9 @@ public class RegisterActivity extends AppCompatActivity {
 
     private void addControls() {
         btnLogin=findViewById(R.id.btnLogin1);
-        edtName=findViewById(R.id.edtName);
         edtEmail=findViewById(R.id.edtEmail1);
         edtPass=findViewById(R.id.edtPass1);
+
+        mAuth=FirebaseAuth.getInstance();
     }
 }
