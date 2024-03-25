d278b50b961d95cb623f77656a16bccc6c8e0e68
Phi Long <philongt474@gmail.com>
2024-03-25 20:56:38 +0700

    Phi Long commit (hoàn thiện)

--- app/src/main/java/com/example/doan/Activity/CartActivity.java
+++ app/src/main/java/com/example/doan/Activity/CartActivity.java
@@ -1,10 +1,13 @@
 package com.example.doan.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import com.example.doan.Adapter.CartAdapter;
 import com.example.doan.R;
@@ -46,6 +49,13 @@ public class CartActivity extends BaseActivity {
             @Override
             public void onClick(View v) {
                 finish();
+            }
+        });
+
+        binding.checkOutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(CartActivity.this,"Thanh toán thành công !!!",Toast.LENGTH_SHORT).show();
             }
         });
     }
